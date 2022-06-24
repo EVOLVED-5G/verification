f610041bef274494178d7c0036fcaa99fa2bbde5
andimintra <angela.dimitriou@netcompany-intrasoft.com>
2022-06-24 12:06:47 +0300

    check robot tests

--- tools/pipelines/verification_tests.groovy
+++ tools/pipelines/verification_tests.groovy
@@ -113,8 +113,9 @@ pipeline{
                     steps{
                         sh """
                             docker exec -t netapp_robot bash \
-                            -c "robot ./tests/capif_invoker_tests/dummy-tests.robot; \
-                                robot ./tests/capif_discover_services/discover_services_tests.robot; \
+                            -c "cd ./tests/capif_invoker_tests &&\
+                                robot dummy-tests.robot; &&\
+                                robot ./tests/capif_discover_services/discover_services_tests.robot; &&\
                                 robot ./tests/capif_publish_services/publish_services_tests.robot;"
                         """
                         // robot ./tests/tests/nef_monitoring_events/nef_monitoring_events_tests.robot;
