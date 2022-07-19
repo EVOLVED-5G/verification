bbdc5bde696f1842b6250ce9722dcb88449c3192
andimintra <angela.dimitriou@netcompany-intrasoft.com>
2022-07-19 12:40:57 +0300

    pipeline updated

--- tools/pipelines/verification_tests.groovy
+++ tools/pipelines/verification_tests.groovy
@@ -9,12 +9,10 @@ pipeline{
 
     parameters{
         // nginx-evolved5g.apps-dev.hi.inet
-        // string(name: 'NGINX_HOSTNAME', defaultValue: 'http://localhost:8888', description: 'nginx hostname')
-        // string(name: "Project_name", defaultValue: "drupal", description: "The name of the project, if upgrade is selected." )
         string(name: "NetApp_repo", defaultValue: "dummy-netapp", description: "The name of the repository of the NetApp to be used." )
         string(name: 'ROBOT_DOCKER_IMAGE_NAME', defaultValue: 'dockerhub.hi.inet/dummy-netapp-testing/robot-test-image', description: 'Robot Docker image name')
         string(name: 'ROBOT_DOCKER_IMAGE_VERSION', defaultValue: '1.0', description: 'Robot Docker image version')
-        string(name: 'NEF_HOSTNAME', defaultValue: 'host.docker.internal', description: 'nef services hostname')
+        string(name: 'NEF_HOSTNAME', defaultValue: 'http://localhost:8888', description: 'nef services hostname') 
         string(name: 'CAPIF_HOSTNAME', defaultValue: 'http://openshift.evolved-5g.eu/', description: 'capif services hostname')
     }
 
@@ -24,7 +22,8 @@ pipeline{
         ROBOT_RESULTS_DIRECTORY = "${WORKSPACE}/results"
         NEF_SERVICES_ENDPOINT = ""
         CAPIF_SERVICES_ENDPOINT = "http://openshift.evolved-5g.eu/"
-        // ROBOT_IMAGE_NAME = 'dockerhub.hi.inet/dummy-netapp-testing/robot-test-image'
+        RUN_NEF_LOCALLY = runCapifLocal("${params.NEF_HOSTNAME}")
+        RUN_CAPIF_LOCALLY = runCapifLocal("${params.CAPIF_HOSTNAME}")
     }
 
     stages{
@@ -39,42 +38,71 @@ pipeline{
                     submoduleCfg: [],
                     userRemoteConfigs: [[url: "https://github.com/EVOLVED-5G/${NetApp_repo}.git"]]
                 ])
-                // checkout([$class: 'GitSCM',
-                //     branches: [[name: 'develop']],
-                //     doGenerateSubmoduleConfigurations: false,
-                //     extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "capif-services"]], 
-                //     gitTool: 'Default',
-                //     submoduleCfg: [],
-                //     userRemoteConfigs: [[url: 'https://github.com/EVOLVED-5G/CAPIF_API_Services.git']]
-                // ])
-                // checkout([$class: 'GitSCM',
-                //     branches: [[name: 'main']],
-                //     doGenerateSubmoduleConfigurations: false,
-                //     extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "nef-services"]], 
-                //     gitTool: 'Default',
-                //     submoduleCfg: [],
-                //     userRemoteConfigs: [[url: 'https://github.com/EVOLVED-5G/NEF_emulator.git']]
-                // ])
                 sh '''
                     ls -la
                 '''
             }
         }
 
-        stage("Set Netapp, Nef & Capif locally."){
+        stage("Checkout nef services"){
+            when {
+                expression { RUN_NEF_LOCALLY == 'true' }
+            }
+            steps{
+                checkout([$class: 'GitSCM',
+                    branches: [[name: 'main']],
+                    doGenerateSubmoduleConfigurations: false,
+                    extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "nef-services"]], 
+                    gitTool: 'Default',
+                    submoduleCfg: [],
+                    userRemoteConfigs: [[url: 'https://github.com/EVOLVED-5G/NEF_emulator.git']]
+                ])
+            }
+        }
+
+        stage("Checkout capif services"){
+            when {
+                expression { RUN_CAPIF_LOCALLY == 'true' }
+            }
+            steps{
+                checkout([$class: 'GitSCM',
+                    branches: [[name: 'develop']],
+                    doGenerateSubmoduleConfigurations: false,
+                    extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "capif-services"]], 
+                    gitTool: 'Default',
+                    submoduleCfg: [],
+                    userRemoteConfigs: [[url: 'https://github.com/EVOLVED-5G/CAPIF_API_Services.git']]
+                ])
+            }
+        }
+
+        stage("Set up environment."){
             stages{
                 
-                stage("Setup environment"){
+                stage("Set up capif services."){
+                    when {
+                        expression { RUN_CAPIF_LOCALLY == 'true' }
+                    }
                     steps {
-                        // dir ("./capif-services") {
-                        //     sh 'ls && cd services && ./run.sh'
-                        // }
-                        // dir ("./nef-services") {
-                        //     sh 'ls && make prepare-dev-env && make build && make up && make db-init'
-                        // }
-                        // dir ("./$NetApp_repo") {
-                        //     sh 'ls && ./run.sh'
-                        // }
+                        dir ("./capif-services") {
+                            sh 'ls && cd services && ./run.sh'
+                        }
+                    }
+                }
+
+                stage("Set up nef services."){
+                    when {
+                        expression { RUN_NEF_LOCALLY == 'true' }
+                    }
+                    steps {
+                        dir ("./nef-services") {
+                            sh 'ls && make prepare-dev-env && make build && make up && make db-init'
+                        }
+                    }
+                }
+
+                stage("Set up Robot FW container."){
+                    steps {
                         dir ("${ROOT_DIRECTORY}") {
                             // sh """
                             //     touch .env && echo "ROBOT_IMAGE_NAME=${ROBOT_DOCKER_IMAGE_NAME}:${ROBOT_DOCKER_IMAGE_VERSION}" > .env && echo "NETAPP_NAME=${NetApp_repo}"
@@ -86,6 +114,7 @@ pipeline{
                             // -e NEF_SERVICES_ENDPOINT=${NEF_SERVICES_ENDPOINT}
                             // -e CAPIF_SERVICES_ENDPOINT=${CAPIF_SERVICES_ENDPOINT}
                             // ${ROBOT_DOCKER_IMAGE_NAME}:${ROBOT_DOCKER_IMAGE_VERSION}
+                            // ./tools/
                             withCredentials([usernamePassword(
                                 credentialsId: 'docker_pull_cred',
                                 usernameVariable: 'USER',
@@ -93,7 +122,7 @@ pipeline{
                             )]) {
                                 sh """
                                     docker login --username ${USER} --password ${PASS} dockerhub.hi.inet
-                                    docker build -t netapp_robot_image ./tools/
+                                    docker build -t netapp_robot_image ${ROBOT_DOCKER_IMAGE_NAME}:${ROBOT_DOCKER_IMAGE_VERSION}
                                     docker run -d -t --name netapp_robot \
                                     -e NEF_SERVICES_ENDPOINT=${NEF_HOSTNAME} \
                                     -e CAPIF_SERVICES_ENDPOINT=${CAPIF_HOSTNAME} \
@@ -106,7 +135,7 @@ pipeline{
                                     -v ${WORKSPACE}/results:/opt/robot-tests/results/ \
                                     -v ${WORKSPACE}/tools/credentials.properties:/opt/robot-tests/credentials.properties netapp_robot_image  
                                 """
-                                }
+                            }
                         }
                     }
                 }
@@ -119,11 +148,12 @@ pipeline{
                             docker exec -t netapp_robot bash \
                             -c "robot /opt/robot-tests/tests/capif_invoker_tests/dummy-tests.robot; \
                                 robot /opt/robot-tests/tests/capif_discover_services/discover_services_tests.robot; \
-                                robot /opt/robot-tests/tests/capif_publish_services/publish_services_tests.robot;"
+                                robot /opt/robot-tests/tests/capif_publish_services/publish_services_tests.robot; \
+                                robot /opt/robot-tests/tests/callbacks/callback_tests.robot.robot;"
                         """
-                        // robot ./tests/tests/nef_monitoring_events/nef_monitoring_events_tests.robot;
-                        // robot ./tests/tests/nef_as_sessions/nef_as_sessions_tests.robot;
-                        // robot ./tests/tests/callbacks/callback_tests.robot.robot;
+                        // robot /opt/robot-tests/tests/tests/nef_monitoring_events/nef_monitoring_events_tests.robot;
+                        // robot /opt/robot-tests/tests/tests/nef_as_sessions/nef_as_sessions_tests.robot;
+                        // robot /opt/robot-tests/tests/callbacks/callback_tests.robot.robot;
                     }
                 }
 
@@ -136,14 +166,19 @@ pipeline{
     post{
         always{
             script {
-                dir ("${env.ROOT_DIRECTORY}") {
-                    // echo 'Shutdown all services'
-                    // sh 'docker-compose down -v'
-                    sh 'docker kill netapp_robot && docker rm netapp_robot && docker rmi netapp_robot_image'
+                if(env.RUN_NEF_LOCALLY == 'true'){
+                    dir ("${env.ROOT_DIRECTORY}/nef-services") {
+                        echo 'Shutdown all nef services'
+                        sh 'docker-compose down -v'
+                    }
                 }
-                // dir ("./$NetApp_repo") {
-                //     sh './cleanup_docker_containers.sh'
-                // }
+                if(env.RUN_CAPIF_LOCALLY == 'true'){
+                    dir ("${env.ROOT_DIRECTORY}/capif-services") {
+                        echo 'Shutdown all capif services'
+                        sh './cleanup_docker_containers.sh'
+                    }
+                }
+                sh 'docker kill netapp_robot && docker rm netapp_robot && docker rmi netapp_robot_image'
             }
 
             script {
