e0dc8822c29b223b42415de2e8feb56d31135000
doubdoub <kon_doubas98@hotmail.com>
2022-11-17 16:07:50 +0200

    Refined Verification Tests

--- resources/common/basicFunctions.robot
+++ resources/common/basicFunctions.robot
@@ -2,8 +2,6 @@
 Documentation   This test file contains the basic register requests from netApp to Capif API.
 # Library         RequestsLibrary
 # Library         Collections
-Library         /opt/robot-tests/pythonnetapp/apf_to_capif.py
-Library         /opt/robot-tests/pythonnetapp/netapp_to_capif.py
 Library         OperatingSystem
 Variables       /opt/robot-tests/libraries/ConfigVariables.py  CONFIG  /opt/robot-tests/credentials.properties
 
@@ -14,93 +12,3 @@ ${already_exists}           409
 ${forbidden}                403
 
 *** Keywords ***
-
-DummyNetApp_register
-
-    [Arguments]    ${CONFIG.credentials.capif_ip}    ${CONFIG.credentials.capif_port}    ${CONFIG.credentials.invoker_username}    ${CONFIG.credentials.invoker_password}    ${CONFIG.credentials.invoker_role}    ${CONFIG.credentials.invoker_description}
-
-    ${resp}=       register_netapp_to_capif    ${CONFIG.credentials.capif_ip}    ${CONFIG.credentials.capif_port}    ${CONFIG.credentials.invoker_username}    ${CONFIG.credentials.invoker_password}    ${CONFIG.credentials.invoker_role}    ${CONFIG.credentials.invoker_description}
-
-    Set Global Variable    ${api_invoker_id}    ${resp}
-    
-    [Return]       ${resp}
-    
-Apf_register 
-
-    [Arguments]    ${CONFIG.credentials.capif_ip}    ${CONFIG.credentials.capif_port}    ${CONFIG.credentials.apf_username}    ${CONFIG.credentials.apf_password}    ${CONFIG.credentials.apf_role}    ${CONFIG.credentials.apf_description}
-
-    Copy File      /opt/robot-tests/pythonnetapp/service_api_description.json    .
-
-    ${resp}=       register_apf_to_capif    ${CONFIG.credentials.capif_ip}    ${CONFIG.credentials.capif_port}    ${CONFIG.credentials.apf_username}    ${CONFIG.credentials.apf_password}    ${CONFIG.credentials.apf_role}    ${CONFIG.credentials.apf_description}
-
-    Set Global Variable    ${api_apf_id}    ${resp}
-    
-    [Return]       ${resp}
-
-DummyNetApp_already_registered
-
-    ${output}=         Run Keyword And Expect Error    *    DummyNetApp_register    ${CONFIG.credentials.capif_ip}    ${CONFIG.credentials.capif_port}    ${CONFIG.credentials.invoker_username}    ${CONFIG.credentials.invoker_password}    ${CONFIG.credentials.invoker_role}    ${CONFIG.credentials.invoker_description}
-
-    Should contain     ${output}    ${already_exists}
-
-
-Token_case_invoker 
-
-    ${resp}=    netapp_to_capif.get_capif_token   ${CONFIG.credentials.capif_ip}    ${CONFIG.credentials.capif_port}    ${CONFIG.credentials.invoker_username}    ${CONFIG.credentials.invoker_password}    ${CONFIG.credentials.invoker_role}
-
-    [Return]    ${resp}
-
-
-Token_case_apf 
-
-    ${resp}=    apf_to_capif.get_capif_token   ${CONFIG.credentials.capif_ip}    ${CONFIG.credentials.capif_port}    ${CONFIG.credentials.apf_username}    ${CONFIG.credentials.apf_password}    ${CONFIG.credentials.apf_role}
-
-    [Return]    ${resp}
-
-
-OnBoard_DummyNetApp_invoker
-    
-    Copy File         /opt/robot-tests/pythonnetapp/invoker_details.json    .
-
-    ${access_token}=  netapp_to_capif.get_capif_token   ${CONFIG.credentials.capif_ip}    ${CONFIG.credentials.capif_port}    ${CONFIG.credentials.invoker_username}    ${CONFIG.credentials.invoker_password}    ${CONFIG.credentials.invoker_role}
-
-    ${resp}=          netapp_to_capif.onboard_netapp_to_capif   ${CONFIG.credentials.capif_ip}    ${CONFIG.credentials.capif_port}    ${access_token}
-
-    [Return]          ${resp}
-
-
-OnBoard_DummyNetApp_apf
-
-    # Run keyword       MoveFile
-    
-    ${access_token}=  apf_to_capif.get_capif_token   ${CONFIG.credentials.capif_ip}    ${CONFIG.credentials.capif_port}    ${CONFIG.credentials.invoker_username}    ${CONFIG.credentials.invoker_password}    ${CONFIG.credentials.invoker_role}
-
-    ${resp}=          netapp_to_capif.onboard_netapp_to_apf   ${CONFIG.credentials.capif_ip}    ${CONFIG.credentials.capif_port}    ${access_token}
-
-    [Return]          ${resp}
-
-
-Invoker_already_registered
-
-    ${output}=         Run Keyword And Expect Error    *    OnBoard_DummyNetApp_invoker
-
-    Should contain     ${output}    ${forbidden}
-
-
-Publish services
-
-    [Arguments]    ${CONFIG.credentials.capif_ip}    ${CONFIG.credentials.capif_port}    ${api_invoker_id}    ${access_token}
-     
-    ${resp}=       apf_to_capif.publish_service_api_to_capif    ${CONFIG.credentials.capif_ip}    ${CONFIG.credentials.capif_port}    ${api_invoker_id}    ${access_token}
-
-    [Return]       ${resp}
-
-
-Clean Test Information By HTTP Requests
-
-    [Arguments]    ${NGINX_HOSTNAME}
-
-    # ${resp}=       DELETE On Session      jwtsession    /testdata
-
-    # Should Be Equal As Strings    ${resp.status_code}    200
-
---
+++ tools/pipelines/refined_verification_tests.groovy
@@ -0,0 +1,226 @@
+package pipelines
+
+pipeline{
+
+    agent { node { label 'evol5-slave' }  }
+
+    parameters{
+        string(name: "NetApp_repo", defaultValue: "dummy-netapp", description: "The name of the repository of the NetApp to be used." )
+        string(name: 'ROBOT_DOCKER_IMAGE_NAME', defaultValue: 'dockerhub.hi.inet/dummy-netapp-testing/robot-test-image', description: 'Robot Docker image name')
+        string(name: 'ROBOT_DOCKER_IMAGE_VERSION', defaultValue: '1.0', description: 'Robot Docker image version')
+    }
+
+    environment {
+        ROOT_DIRECTORY = "${WORKSPACE}/"
+        ROBOT_TESTS_DIRECTORY = "${WORKSPACE}/tests"
+        ROBOT_RESULTS_DIRECTORY = "${WORKSPACE}/results"
+        NEF_SERVICES_ENDPOINT = ""
+        CAPIF_SERVICES_ENDPOINT = "http://openshift.evolved-5g.eu/"
+        RUN_NEF_LOCALLY = "true"
+        RUN_CAPIF_LOCALLY = "true"
+    }
+
+    stages{
+
+        stage("Checkout code"){
+            steps{
+                checkout([$class: 'GitSCM',
+                          branches: [[name: 'main']],
+                          doGenerateSubmoduleConfigurations: false,
+                          extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "$NetApp_repo"]],
+                          gitTool: 'Default',
+                          submoduleCfg: [],
+                          userRemoteConfigs: [[url: "https://github.com/EVOLVED-5G/${NetApp_repo}.git"]]
+                ])
+            }
+        }
+
+        stage("Checkout nef services"){
+            when {
+                expression { RUN_NEF_LOCALLY == 'true' }
+            }
+            steps{
+                checkout([$class: 'GitSCM',
+                          branches: [[name: 'main']],
+                          doGenerateSubmoduleConfigurations: false,
+                          extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "nef-services"]],
+                          gitTool: 'Default',
+                          submoduleCfg: [],
+                          userRemoteConfigs: [[url: 'https://github.com/EVOLVED-5G/NEF_emulator.git']]
+                ])
+            }
+        }
+
+        stage("Checkout capif services"){
+            when {
+                expression { RUN_CAPIF_LOCALLY == 'true' }
+            }
+            steps{
+                checkout([$class: 'GitSCM',
+                          branches: [[name: 'develop']],
+                          doGenerateSubmoduleConfigurations: false,
+                          extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "capif-services"]],
+                          gitTool: 'Default',
+                          submoduleCfg: [],
+                          userRemoteConfigs: [[url: 'https://github.com/EVOLVED-5G/CAPIF_API_Services.git']]
+                ])
+            }
+        }
+
+        stage("Set up environment."){
+            stages{
+
+                stage("Set up capif services."){
+                    when {
+                        expression { RUN_CAPIF_LOCALLY == 'true' }
+                    }
+                    steps {
+                        dir ("./capif-services") {
+                            sh """
+                                ls -la && cd services
+                                sed -i "s/image: mongo:6.0.2/image: mongo:4.4.17/g" docker-compose.yml
+                                ./run.sh
+                            """
+                        }
+                    }
+                }
+
+                stage("Set up nef services."){
+                    when {
+                        expression { RUN_NEF_LOCALLY == 'true' }
+                    }
+                    steps {
+                        dir ("./nef-services") {
+                            sh """
+                                sed -i "s/3.9.7/3.9.15/g" backend/app/pyproject.toml
+                                sed -i "s/EXTERNAL_NET=false/EXTERNAL_NET=true/g" env-file-for-local.dev
+                                make prepare-dev-env
+                                make build
+                                make upd
+                            """
+                        }
+                    }
+                }
+
+                stage("Set up dummy netapp."){
+                    steps {
+                        dir ("$NetApp_repo") {
+                            sh """
+                                sed -i 's+"capif_callback_url": "http://192.168.1.13:5000"+"capif_callback_url": "http://host.docker.internal:8086"+g' pythonnetapp/capif_registration.json
+                                ./run.sh
+                            """
+                        }
+                    }
+                }
+
+            }
+
+        }
+        stage("Verify setup"){
+            stages{
+                stage("Verify Nef registration to Capif"){
+                    steps {
+                        dir ("./nef-services") {
+                            sh """
+                                docker-compose ps
+                                ls -l backend/app/app/core/certificates/
+                            """
+                        }
+                    }
+
+                }
+                stage("Verify Netapp registration to Capif"){
+                    steps {
+                        dir("$NetApp_repo") {
+                            sh """
+                                docker-compose ps
+                                ls -l pythonnetapp/capif_onboarding
+                            """
+                        }
+                    }
+                }
+            }
+        }
+        stage ("Setup Robot FW && Run tests"){
+            stages{
+                stage("Set up Robot FW container."){
+                    steps {
+                        dir ("${ROOT_DIRECTORY}") {
+                            withCredentials([usernamePassword(
+                                    credentialsId: 'docker_pull_cred',
+                                    usernameVariable: 'USER',
+                                    passwordVariable: 'PASS'
+                            )]) {
+                                sh """
+                                    docker run -d -t --name netapp_robot \
+                                        --rm --add-host capifcore:host-gateway \
+                                        -v ${ROOT_DIRECTORY}/${NetApp_repo}/capif_callback_server:/opt/robot-tests/capif-callback \
+                                        -v ${ROOT_DIRECTORY}/${NetApp_repo}/nef_callback_server:/opt/robot-tests/nef-callback \
+                                        -v ${ROOT_DIRECTORY}/${NetApp_repo}/pythonnetapp:/opt/robot-tests/pythonnetapp \
+                                        -v ${WORKSPACE}/tests:/opt/robot-tests/tests/ \
+                                        -v ${WORKSPACE}/libraries:/opt/robot-tests/libraries/ \
+                                        -v ${WORKSPACE}/resources:/opt/robot-tests/resources/ \
+                                        -v ${WORKSPACE}/results:/opt/robot-tests/results/ \
+                                        -v ${WORKSPACE}/tools/credentials.properties:/opt/robot-tests/credentials.properties ${ROBOT_DOCKER_IMAGE_NAME}:${ROBOT_DOCKER_IMAGE_VERSION}  
+                                """
+                            }
+                        }
+                    }
+                }
+                stage("Run test cases."){
+                    steps{
+                        sh """
+                            docker exec -t netapp_robot bash -c "ls -la"
+                        """
+                        sh """
+                            docker exec -t netapp_robot bash \
+                            -c "robot /opt/robot-tests/tests/netapp_nef_monitoring/monitoring_verification.robot; \
+                                python3 /opt/robot-tests/libraries/scenario/clear_scenario.py capifcore
+                                robot /opt/robot-tests/tests/netapp_nef_sessionqos/sessionqos_verification.robot"
+                        """
+                    }
+                }
+
+            }
+        }
+    }
+
+    post{
+        always{
+            script {
+                if(env.RUN_NEF_LOCALLY == 'true'){
+                    dir ("${env.ROOT_DIRECTORY}/nef-services") {
+                        echo 'Shutdown all nef services'
+                        sh 'make down-v'
+                    }
+                }
+                if(env.RUN_CAPIF_LOCALLY == 'true'){
+                    dir ("${env.ROOT_DIRECTORY}/capif-services/services") {
+                        echo 'Shutdown all capif services'
+                        sh './clean_capif_docker_services.sh'
+                    }
+                }
+                sh """
+                    docker kill netapp_robot
+                    docker rmi ${ROBOT_DOCKER_IMAGE_NAME}:${ROBOT_DOCKER_IMAGE_VERSION}
+                """
+                dir ("$NetApp_repo") {
+                    sh """
+                        docker-compose down -v
+                        docker network rm demo-network
+                    """
+                }
+            }
+
+            script {
+                echo "Deleting directories."
+                cleanWs deleteDirs: true
+            }
+            echo "Done."
+        }
+        success{
+            echo "Test ran successfully."
+        }
+    }
+
+}
