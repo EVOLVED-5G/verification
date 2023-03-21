25e146e098683047bfa5b32be509209fdcb9ca9c
doubdoub <kon_doubas98@hotmail.com>
2023-03-21 10:37:23 +0200

    some more fixes

--- tools/checks/nef_check.sh
+++ tools/checks/nef_check.sh
@@ -3,8 +3,7 @@
 set -e
 
 CERTS_PATH="backend/app/app/core/certificates"
-check_num_of_files=$(ls $CERTS_PATH | wc -l)
-check_private_key=$(openssl rsa -in $CERTS_PATH/private.key -check 2>&1)
+check_num_of_files=$(ls $CERTS_PATH | grep -v gitkeep | wc -l)
 
 if [[ ${check_num_of_files} -eq 12 ]]; then
    echo "All files are here";
